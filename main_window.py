94cc3f509b9d784b9355a2a923ad8bbc88544175
Zach <zachmichael14@gmail.com>
2026-01-31 13:10:43 -0600

    Switch interface to tabbed format

--- main_window.py
+++ main_window.py
@@ -1,4 +1,9 @@
-from PySide6.QtWidgets import QMainWindow, QWidget
+from PySide6.QtWidgets import (
+    QLabel,
+    QMainWindow,
+    QTabWidget,
+    QWidget,
+)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -7,5 +12,8 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("~ MPooler ~")
         self.resize(800, 600)
 
-        central_widget = QWidget(self)
+        central_widget = QTabWidget(self)
         self.setCentralWidget(central_widget)
+
+        tab_placeholder1 = QLabel("Am tab")
+        central_widget.addTab(tab_placeholder1, "Placeholder")
