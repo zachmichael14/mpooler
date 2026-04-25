f977775315b3e34f4cb4460d26fbead879a70a80
Zach <zachmichael14@gmail.com>
2026-04-25 12:59:26 -0500

    Add basic deck model

---
+++ models/deck.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass, field
+from uuid import UUID
+
+from models.color_set import ColorSet
+
+# TODO: Maybe format should be it's own model? That seems like it might 
+# encapsulate rules and legality better.
+# I only play commmander and eternal/casual, so it's not necessary for now
+@dataclass
+class Deck:
+    deck_id: UUID
+    name: str
+    format: str
+    colors: ColorSet = field(default_factory=ColorSet)
+
+
