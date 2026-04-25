46560ad89d5e09cd21bdb830d758760a843f4cae
Zach <zachmichael14@gmail.com>
2026-04-25 18:26:26 -0500

    Add Location ORM

---
+++ data/location.py
@@ -0,0 +1,17 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from data.base import Base
+
+class LocationORM(Base):
+    """
+    The physical location of a card.
+    """
+    __tablename__ = "locations"
+
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    type: Mapped[str] = mapped_column(String, nullable=False) # LocationType
+
+    # String keeps theses lists forward-references for typing
+    cards: Mapped[list["CardORM"]] = relationship(back_populates="location")
+    decks: Mapped[list["DeckORM"]] = relationship(back_populates="location")
