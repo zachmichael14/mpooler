c12310bd62d733ce142fd1bed8cf1228184998cd
Zach <zachmichael14@gmail.com>
2026-04-25 18:59:22 -0500

    Add helper methods for serialization

--- models/color_set.py
+++ models/color_set.py
@@ -15,3 +15,12 @@ class ColorSet:
     """TODO: How to handle colorless? It's not a color, so adding it here seems inappropriate. Maybe just a lack of colors (length == 0) is adequate"""
     # default_factory creates an empty set of ManaColors upon instantiation
     colors: set[ManaColor] = field(default_factory=set)
+
+    def to_string_list(self) -> list[str]:
+        """Return the ColorSet as a list of color letters."""
+        return [color.value for color in self.colors]
+    
+    @classmethod
+    def from_string_list(cls, values: list[str]) -> "ColorSet":
+        """Create a ColorSet from a list of color letters."""
+        return cls(colors={ManaColor(value) for value in values})
