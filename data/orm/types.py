15d809f0215759bbe9a9fe5e242a30724d1e6966
Zach <zachmichael14@gmail.com>
2026-04-25 19:44:43 -0500

    Add type decorator for new condition enum

--- data/orm/types.py
+++ data/orm/types.py
@@ -2,6 +2,7 @@ import sqlalchemy.types as types
 from sqlalchemy import String
 
 from models.color_set import ColorSet
+from models.condition import Condition
 
 class ColorSetType(types.TypeDecorator):
     """
@@ -29,3 +30,24 @@ class ColorSetType(types.TypeDecorator):
         if not value:
             return ColorSet()
         return ColorSet.from_string_list(value.split(","))
+
+class ConditionType(types.TypeDecorator):
+    """"""
+    impl = String
+    cache_ok = True
+
+    def process_bind_param(self,
+                           value: Condition | None, 
+                           dialect) -> str | None:
+        """When writing to database, convert Condition to string."""
+        if value is None:
+            return None
+        return value.value
+    
+    def process_result_value(self,
+                             value: str | None,
+                             dialect) -> Condition | None:
+        """When reading from a database, convert string to Condition."""
+        if not value:
+            return None
+        return Condition(value)
