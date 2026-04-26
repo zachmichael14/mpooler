0d08fce5f45cb47ce15263157bc79c9e813939f6
Zach <zachmichael14@gmail.com>
2026-04-25 19:06:27 -0500

    Add basic Deck ORM

--- data/orm/deck.py
+++ data/orm/deck.py
@@ -1,10 +1,12 @@
 import uuid
 
 from sqlalchemy import ForeignKey, String, Uuid
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from data.orm.base import Base
+from data.orm.types import ColorSetType
 from models.color_set import ColorSet
+
 
 class DeckORM(Base):
     __tablename__ = "decks"
@@ -13,4 +15,8 @@ class DeckORM(Base):
                                                    ForeignKey("locations.id"), 
                                                    nullable=False)
     name: Mapped[str] = mapped_column(String, nullable=False)
-    colors: Mapped[ColorSet] = mapped_column()
+    colors: Mapped[ColorSet] = mapped_column(ColorSetType, nullable=True)
+    format: Mapped[str] = mapped_column(String)
+
+    location: Mapped["LocationORM"] = relationship(back_populates="decks")
+    card: Mapped["CardORM"] = relationship(back_populates="deck")
