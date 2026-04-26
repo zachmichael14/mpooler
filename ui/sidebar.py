89845fd4de07db7d1601b776071e8c8614603a7f
Zach <zachmichael14@gmail.com>
2026-04-26 15:23:04 -0500

    Add sub options for collection mgmt

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -1,5 +1,7 @@
 from PySide6.QtCore import Signal
 from PySide6.QtWidgets import (
+    QListWidget,
+    QListWidgetItem,
     QPushButton,
     QVBoxLayout,
     QWidget
@@ -8,6 +10,7 @@ from PySide6.QtWidgets import (
 
 class Sidebar(QWidget):
     sig_button_clicked = Signal(str)
+    sig_sub_option_changed = Signal(str)
 
     def __init__(self):
         super().__init__()
@@ -20,14 +23,30 @@ class Sidebar(QWidget):
         self._collection_button.clicked.connect(self._on_collection_clicked)
         layout.addWidget(self._collection_button)
 
+        self._sub_options = QListWidget()
+        layout.addWidget(self._sub_options)
+
+        locations = QListWidgetItem("Storage Locations")
+        self._sub_options.addItem(locations)
+
+        cards = QListWidgetItem("Cards")
+        self._sub_options.addItem(cards)
+
+        decks = QListWidgetItem("Decks")
+        self._sub_options.addItem(decks)
+        self._sub_options.currentItemChanged.connect(self._on_sub_option_changed)
+
         self._scan_button = QPushButton("Card Scanner")
         self._scan_button.setCheckable(True)
         self._scan_button.setAutoExclusive(True)
         self._scan_button.clicked.connect(self._on_scan_clicked)
         layout.addWidget(self._scan_button)
 
-    
-    def _on_collection_clicked(self):
+    def _on_sub_option_changed(self, current_option: QListWidgetItem) -> None:
+        selection = current_option.text().casefold()
+        self.sig_sub_option_changed.emit(selection)
+
+    def _on_collection_clicked(self) -> None:
         self.sig_button_clicked.emit("collection")
 
     def _on_scan_clicked(self) -> None:
