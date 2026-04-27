53d0bdc2e5ddd6d65dc62de028ede1d9829ab322
Zach <zachmichael14@gmail.com>
2026-04-26 21:56:14 -0500

    Build location form

--- ui/location_panel.py
+++ ui/location_panel.py
@@ -1,8 +1,13 @@
 from PySide6.QtWidgets import (
+    QComboBox,
+    QFormLayout,
+    QLineEdit,
     QWidget
 )
 
+from models.location_type import LocationType
 from ui.base_collection_panel import BaseCollectionPanel
+
 
 
 class LocationPanel(BaseCollectionPanel):
@@ -11,7 +16,22 @@ class LocationPanel(BaseCollectionPanel):
         super().__init__()
 
     def _build_object_form(self) -> QWidget:
-        return QWidget()
+        form = QWidget()
+        layout = QFormLayout(form)
+
+        self._name = QLineEdit()
+        
+        self._type = QComboBox()
+        for type in LocationType:
+            #userData stores metadata, essentially
+            self._type.addItem(type.value, userData=type)
+
+        layout.addRow("Name", self._name)
+        layout.addRow("Type", self._type)
+        return form
+
+    def _build_left_pane(self) -> QWidget:
+        pass
 
     def _clear_form(self) -> None:
         pass
